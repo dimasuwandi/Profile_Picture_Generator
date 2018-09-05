8f1f86f66c052aa43e8d078a71cb56aa4c9e6b34
dimasuwandi <muhammad_dimas95@yahoo.com>
2018-09-05 15:40:29 +0700

    Profil_Picture_Generator

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+
+
+class App extends Component {
+    constructor(){
+        super();
+        this.state={isi1: ""}
+    }
+
+    klik (){
+        var a = this.refs.nilai1.value;
+        var gambar = `https://robohash.org/${a}`
+
+        this.setState({
+            isi1:gambar
+        })
+    }
+        
+    render(){
+        return(
+          <center>
+            <div id="no1" >
+                <input id="ipt" type="text" ref="nilai1" placeholder="Ketik nama Anda..."></input><br/><br/>
+                <button id="btn" class="btn btn-success" onClick={()=>{this.klik()}} >Buat Profile Picture!</button>
+                <br/><br/>
+                <img id="img" src={this.state.isi1}></img>
+            </div>
+          </center>
+        )
+    }
+}
+export default App;
+
+
